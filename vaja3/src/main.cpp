752974aa61de629a83cee9542169a0d0e462cd48
leonbl <leon.blazic@gmail.com>
2021-11-29 14:02:36 +0100

    serijski

--- vaja3/src/main.cpp
+++ vaja3/src/main.cpp
@@ -1,12 +1,29 @@
 #include <Arduino.h>
-char c = 65;
 
+int indx=0;
 void setup() {
   Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
 void loop() {
-  Serial.println(c++);
-  delay(100);
-  if(c > 90) c=65;
+  if(Serial.available() > 0){
+    char c = Serial.read(); 
+    Serial.println(c);
+    if( c=='#'){
+      Serial.println("V zanki:");
+      for(int n = 10; n<14; n++){
+        while(Serial.available() == 0);
+        c =  Serial.read();
+        Serial.print(n);
+        Serial.print(' ');
+        Serial.println((int)c);
+        if(c=='0') digitalWrite(n, HIGH);
+        else if(c=='1')  digitalWrite(n, LOW);      
+      }
+    }
+  }
 }
